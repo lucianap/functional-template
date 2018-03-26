2fcbb4ddce4c6843799900f453bbb3b2d572e358
Luciana P <lucianasl@gmail.com>
2018-03-25 23:57:18 -0300

    Ex 7

--- src/exercise7.clj
+++ src/exercise7.clj
@@ -8,13 +8,13 @@
 
 (defmulti fmap (fn [function collection] [(type function)(type collection)]))
 
-(defmethod fmap [clojure.core$inc clojure.lang.PersistentVector] [function collection]
+(defmethod fmap [:default clojure.lang.PersistentVector] [function collection]
   (map function collection))
 
-(defmethod fmap [clojure.core$inc clojure.lang.IPersistentMap] [function collection]
+(defmethod fmap [:default clojure.lang.IPersistentMap] [function collection]
   (into {} (for [[key val] collection] [key (function val)])))
 
-(defmethod fmap [clojure.core$inc clojure.lang.PersistentList] [function collection]
+(defmethod fmap [:default clojure.lang.PersistentList] [function collection]
   (map function collection))
 
 
